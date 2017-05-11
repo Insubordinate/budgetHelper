12e905ad835c03092a13a2f045dc8b258f823ef0
Axl Cruz-Garcia <axl.cruz8344@gmail.com>
2017-05-10 23:30:01 -0400

    Added to the skeleton

--- budgetTracker.rb
+++ budgetTracker.rb
@@ -4,3 +4,18 @@ puts "Welcome to the app"
 puts "1. Check your current savings"
 puts "2. Plan a new project"
 puts "3. Check your budget"
+puts "4. Exit the app"
+
+input = gets.chomp
+
+
+case 
+when input =="1"
+	puts "nope"
+when input == 2
+	puts "nope"
+when input == 3
+	puts "nope"
+when input == 4
+	exit
+end
